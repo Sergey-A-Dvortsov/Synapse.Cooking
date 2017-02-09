bbf819972107e8ef85682a82a08c3036cc561e45
Sergey-Dvortsov <dvortsov@list.ru>
2017-02-09 22:40:11 +0300

    FirstLog changed

---
+++ FirstLog/Connection.cs
@@ -0,0 +1,99 @@
+﻿using MoreLinq;
+using StockSharp.Algo;
+using StockSharp.BusinessEntities;
+using StockSharp.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FirstLog
+{
+    public class Connection : BaseLogReceiver
+    {
+
+        private Connector _connector;
+        static string _portfolioName;
+        static Portfolio _portfolio;
+        static string _securityId;
+        static Security _security;
+
+        public Connection(Connector connector, string portfolioName, string securityId)
+        {
+            _connector = connector;
+            _portfolioName = portfolioName;
+            _securityId = securityId;
+        }
+
+        /// <summary>
+        /// Устанавливает соединение
+        /// </summary>
+        public void Connect()
+        {
+            _connector.Connected += OnConnected;
+            _connector.Disconnected += OnDisconnected;
+            _connector.NewPortfolios += OnNewPortfolios;
+            _connector.NewSecurities += OnNewSecurities;
+            _connector.Connect();
+        }
+
+        /// <summary>
+        /// Разрывает соединение
+        /// </summary>
+        public void Disconnect()
+        {
+            _connector.Connected -= OnConnected;
+            _connector.Disconnected -= OnDisconnected;
+            _connector.NewPortfolios -= OnNewPortfolios;
+            _connector.NewSecurities -= OnNewSecurities;
+            _connector.Disconnect();
+        }
+
+        private void OnConnected()
+        {
+            //  Добавляем в журнал информационное сообщение 
+            this.AddInfoLog("Соединение установлено!");
+        }
+
+        private void OnDisconnected()
+        {
+            //  Добавляем в журнал информационное сообщение 
+            this.AddInfoLog("Соединение разорвано!");
+        }
+
+        private void OnNewPortfolios(IEnumerable<Portfolio> portfolios)
+        {
+            portfolios.ForEach(portfolio =>
+            {
+                //  Добавляем в журнал отладочное сообщение 
+                this.AddDebugLog("Получен портфель: {0}", portfolio.Name);
+            });
+        }
+
+        private void OnNewSecurities(IEnumerable<Security> securities)
+        {
+            try
+            {
+                securities.ForEach(s =>
+                {
+                    if (s.Id == _securityId)
+                    {
+                        _security = s;
+                        //  Добавляем в журнал отладочное сообщение 
+                        this.AddDebugLog("Получен инструмент: {0}", s.Id);
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                //  Добавляем в журнал сообщение об ошибке
+                this.AddErrorLog(ex);
+            }
+
+        }
+
+    }
+
+}
---
+++ FirstLog/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace Synapse.Cooking.FirstLog
+{
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+
+    using Ecng.Collections;
+    using Ecng.Common;
+
+    using MoreLinq;
+
+    using StockSharp.Algo;
+    using StockSharp.Quik;
+    using StockSharp.BusinessEntities;
+    using StockSharp.Messages;
+    using System.Text;
+    using StockSharp.Logging;
+    using global::FirstLog;
+
+    class Program : BaseLogReceiver
+    {
+        static Connector _trader;
+        static AutoResetEvent _handler;
+        static LogManager _logManager;
+        static Connection _connection;
+
+
+        static void Main(string[] args)
+        {
+            _trader = new QuikTrader();
+
+            // объкт, управляющий логгированием
+            _logManager = new LogManager();
+
+            // слушатель - устройство вывода, куда будут записываться сообщения
+
+            // слушатели: 
+            // консоль
+            _logManager.Listeners.Add(new ConsoleLogListener());
+            // окно вывода
+            _logManager.Listeners.Add(new DebugLogListener());
+            // файл
+            var fileListener = new FileLogListener
+            {
+                Append = true,               // true - добавляет сообщения в конец существующего файла, false - перезаписывает существующий файл
+                Extension = "log",           // расширение файла лога
+                FileName = "FirstLog",       // имя файла лога
+                MaxLength = 1,               // максимальная длинна файла лога 
+                LogDirectory = "Logs",       // максимальная длинна файла лога 
+                SeparateByDates = SeparateByDateModes.FileName  // режим разделения логов по датам
+            };
+
+            _logManager.Listeners.Add(fileListener);
+
+            // добавляем источник логов. В данном случае это коннектор.
+            // Роль истоника логов может выполнять любой класс, наследующий от BaseLogReceiver, или реализующий интерфейс ILogSource
+            _logManager.Sources.Add(_trader);
+
+            _connection = new Connection(_trader, "NL0011100043", "SBER@QJSIM");
+
+
+            // Уровни логирования
+            // Off - логи не выводятся
+            // Error - выводятся только сообщения об ошибках
+            // Warning - выводятся предупреждения и сообщения об ошибках
+            // Info - выводятся информационные сообщения, предупреждения и сообщения об ошибках
+            // Debug - выводятся все сообщения
+
+            _connection.LogLevel = LogLevels.Debug;
+
+            // добавляем еще один источник логов. 
+            _logManager.Sources.Add(_connection);
+
+            _connection.Connect();
+      
+            Console.Read();
+
+            _connection.Disconnect();
+
+        }
+
+
+    }
+}
